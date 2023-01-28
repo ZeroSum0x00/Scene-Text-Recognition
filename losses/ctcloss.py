6718e4fa687f3b1d9df0a918f3c66e2b65d36364
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-01-28 21:30:57 +0700

    Update ctcloss.py

--- losses/ctcloss.py
+++ losses/ctcloss.py
@@ -9,10 +9,12 @@ class CTCLoss(tf.keras.losses.Loss):
                  name=None):
         super(CTCLoss, self).__init__(reduction=reduction, name=name)
         self.y_true_padding_const = y_true_padding_const
-      
+        self.invariant_name = "CTCLoss"
+        self.coefficient = 1
+
     def __call__(self, y_true, y_pred, label_length):
         y_true_length = tf.expand_dims(label_length, axis=1)
         y_pred_shape = tf.shape(y_pred)
         y_pred_length = tf.fill((y_pred_shape[0], 1), y_pred_shape[1])
         loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, y_pred_length, y_true_length)
-        return tf.squeeze(loss)
+        return tf.squeeze(loss) * self.coefficient
