2c6bc795c0463fca37e612cecc9dcaa0fbf7066b
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-02-19 11:09:30 +0700

    Update ctcloss.py

--- losses/ctcloss.py
+++ losses/ctcloss.py
@@ -15,6 +15,6 @@ class CTCLoss(tf.keras.losses.Loss):
     def __call__(self, y_true, y_pred, label_length):
         y_true_length = tf.expand_dims(label_length, axis=1)
         y_pred_shape = tf.shape(y_pred)
-        y_pred_length = tf.fill((y_pred_shape[0], 1), y_pred_shape[1])
+        y_pred_length = tf.fill(dims=[y_pred_shape[0], 1], value=y_pred_shape[1])
         loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, y_pred_length, y_true_length)
         return tf.squeeze(loss) * self.coefficient
