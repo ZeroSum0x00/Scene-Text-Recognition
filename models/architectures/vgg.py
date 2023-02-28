be8ccb81a22ebd3efafe3e2b53c6bb9640d834f3
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-02-28 16:47:46 +0700

    Update vgg.py

--- models/architectures/vgg.py
+++ models/architectures/vgg.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras import Sequential
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Activation
@@ -7,49 +8,37 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import SpatialDropout2D
 
 
-class VGG(tf.keras.layers.Layer):
-    def __init__(self, num_filters, *args, **kwargs):
-        super(VGG, self).__init__(*args, **kwargs)
-        self.num_filters = num_filters
+def VGG_FeatureExtractor(num_filters, input_shape=(32, 200, 3)):
+    f0, f1, f2, f3 = num_filters
+    img_input = Input(shape=input_shape)
     
-    def build(self, input_shape):
-        f0, f1, f2, f3 = self.num_filters
-        self.block0 = Sequential([
-                        Conv2D(filters=f0, kernel_size=(3, 3), strides=(1, 1), padding='same'),
-                        Activation('relu'),
-                        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
-                        SpatialDropout2D(0.35)
-        ])
-        self.block1 = Sequential([
-                        Conv2D(filters=f1, kernel_size=(3, 3), strides=(1, 1), padding='same'),
-                        Activation('relu'),
-                        MaxPooling2D(pool_size=(2, 2), strides=(2, 2)),
-                        SpatialDropout2D(0.35)
-        ])
-        self.block2 = Sequential([
-                        Conv2D(filters=f2, kernel_size=(3, 3), strides=(1, 1), padding='same'),
-                        Activation('relu'),
-                        Conv2D(filters=f2, kernel_size=(3, 3), strides=(1, 1), padding='same'),
-                        Activation('relu'),
-                        MaxPooling2D(pool_size=(2, 1), strides=(2, 1)),
-                        SpatialDropout2D(0.35)
-        ])
-        self.block3 = Sequential([
-                        Conv2D(filters=f3, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False),
-                        BatchNormalization(),
-                        Activation('relu'),
-                        Conv2D(filters=f3, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False),
-                        BatchNormalization(),
-                        Activation('relu'),
-                        MaxPooling2D(pool_size=(2, 1), strides=(2, 1)),
-                        SpatialDropout2D(0.35),
-                        Conv2D(filters=f3, kernel_size=(2, 2), strides=(1, 1), padding='valid'),
-                        Activation('relu'),
-                        SpatialDropout2D(0.35)
-        ])
-    def call(self, inputs, training=False):
-        x = self.block0(inputs, training=training)
-        x = self.block1(x, training=training)
-        x = self.block2(x, training=training)
-        x = self.block3(x, training=training)
-        return x
+    x = Conv2D(filters=f0, kernel_size=(3, 3), strides=(1, 1), padding='same')(img_input)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+    x = SpatialDropout2D(0.35)(x)
+
+    x = Conv2D(filters=f1, kernel_size=(3, 3), strides=(1, 1), padding='same')(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(x)
+    x = SpatialDropout2D(0.35)(x)
+    
+    x = Conv2D(filters=f2, kernel_size=(3, 3), strides=(1, 1), padding='same')(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=f2, kernel_size=(3, 3), strides=(1, 1), padding='same')(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(2, 1), strides=(2, 1))(x)
+    x = SpatialDropout2D(0.35)(x)
+
+    x = Conv2D(filters=f3, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters=f3, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D(pool_size=(2, 1), strides=(2, 1))(x)
+    x = SpatialDropout2D(0.35)(x)
+    x = Conv2D(filters=f3, kernel_size=(2, 2), strides=(1, 1), padding='valid')(x)
+    x = Activation('relu')(x)
+    x = SpatialDropout2D(0.35)(x)
+    model = Model(inputs=img_input, outputs=x, name='VGG')
+    return model
