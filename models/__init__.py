b489d7c58206d5bdd5a838e40823c916ebf61ca8
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2023-02-28 09:10:59 +0700

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,4 @@ from .scene_text_recognition import STR
 from .crnn import CRNN
 
 from .layers.label_converter import CTCLabelConverter
-from .architectures import VGG_FeatureExtractor
+from .architectures import VGG
