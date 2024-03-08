ad3de097376c5286117a09d90a8d72abaf2f740b
ZeroSum0x00 <nguyenichthanhtu0509@gmail.com>
2024-03-08 13:18:20 +0700

    fix max length is over pred length

--- losses/ctc_loss.py
+++ losses/ctc_loss.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import losses
+from tensorflow.keras import backend as K
 
 
 class CTCLoss(tf.keras.losses.Loss):
@@ -10,10 +11,20 @@ class CTCLoss(tf.keras.losses.Loss):
         self.invariant_name = "ctc_loss"
         self.coefficient = 1
 
-    def __call__(self, y_true, y_pred, label_length, sample_weight=None):        
-        y_true_length = tf.expand_dims(label_length, axis=1)
+    def __call__(self, y_true, y_pred, label_length, sample_weight=None):
+        if label_length.dtype != tf.int32:
+            label_length = tf.cast(label_length, dtype=tf.int32)
+
         y_pred_shape = tf.shape(y_pred)
-        y_pred_length = tf.fill(dims=[y_pred_shape[0], 1], value=y_pred_shape[1])
-        loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, y_pred_length, y_true_length)
-        # loss = tf.nn.ctc_loss(y_true, y_pred, tf.reshape(y_pred_length, -1), tf.reshape(y_true_length, -1), logits_time_major=False)
-        return tf.reduce_mean(tf.squeeze(loss) * self.coefficient)
+        y_pred_length = tf.fill(dims=[y_pred_shape[0]], value=y_pred_shape[1])
+        
+        sparse_labels = K.ctc_label_dense_to_sparse(y_true, label_length)
+        sparse_labels = tf.cast(sparse_labels, tf.int32)
+
+        y_pred = tf.math.log(tf.transpose(y_pred, perm=[1, 0, 2]) + K.epsilon())
+
+        loss = tf.compat.v1.nn.ctc_loss(inputs=y_pred, 
+                                        labels=sparse_labels, 
+                                        sequence_length=y_pred_length, 
+                                        ignore_longer_outputs_than_inputs=True)
+        return tf.reduce_mean(loss * self.coefficient)
