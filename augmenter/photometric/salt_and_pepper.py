62e529f9ecd436999051f509c8ac5b063c6a6a14
ZeroSum0x00 <88105570+ZeroSum0x00@users.noreply.github.com>
2024-04-03 12:38:36 +0700

    Update salt_and_pepper.py

--- augmenter/photometric/salt_and_pepper.py
+++ augmenter/photometric/salt_and_pepper.py
@@ -65,14 +65,17 @@ class SaltAndPepper:
         self.phi = phi
         
     def __call__(self, image):
-        img = image.copy()
-        dtype = img.dtype
-        intensity_levels = 2 ** (img[0, 0].nbytes * 8)
-        min_intensity, max_intensity = 0, intensity_levels - 1
-        random_image = np.random.choice([min_intensity, 1, np.nan], p=[self.phi / 2, 1 - self.phi, self.phi / 2], size=img.shape)
-        img = img.astype(np.float32) * random_image
-        img = np.nan_to_num(img, nan=max_intensity).astype(dtype)
-        return img
+        try:
+            img = image.copy()
+            dtype = img.dtype
+            intensity_levels = 2 ** (img[0, 0].nbytes * 8)
+            min_intensity, max_intensity = 0, intensity_levels - 1
+            random_image = np.random.choice([min_intensity, 1, np.nan], p=[self.phi / 2, 1 - self.phi, self.phi / 2], size=img.shape)
+            img = img.astype(np.float32) * random_image
+            img = np.nan_to_num(img, nan=max_intensity).astype(dtype)
+            return img
+        except:
+            return image
 
 
 class RandomSaltAndPepper:
